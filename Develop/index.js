293690bfb2da313ad2882e64814b801e6a98ba54
Kristen Groller <groller@rogers.com>
2021-02-06 16:48:50 -0500

    intial questions and Markdown layout

--- Develop/index.js
+++ Develop/index.js
@@ -1,24 +1,19 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const questions = [promptQuestions];
+
+
+//const generateMarkdown = require("./utils/generateMarkdown.js");
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-
-const promptQuestions = () => {
-    return inquirer.prompt([
+const questions = [
+      
       {
       type: 'input',
       name: 'title',
-      message: 'Choose a self explaining name for you project: (Required)',
-      validate: titleInput => {
-        if (titleInput) {        
-          return true;
-        } else {
-          console.log('Please enter your project title!');
-          return false;
-        }
-        }
+      message: 'Choose a self explaining name for you project:',     
+      
       },
       {
           type: "input",
@@ -39,71 +34,48 @@ const promptQuestions = () => {
         type: 'list',
         name: 'licenses',
         message: 'Please select how your project is licensed:',
-        choices: ['need licenses']
+        choices: ['MIT',
+         'GNU()', 
+         'None']
       },
       {
         type: 'confirm',
         name: 'contribute',
         message: 'Are you open to contributions to your project?',
-        default: true
+      
       },
       {
         type: 'input',
         name: 'confirmContribute',
-        message: 'What are your requirements for accepting contributions?',
-        when: ({ confirmContribute }) => {
-          if (confirmContribute) {
-            return true;
-          } else {
-            return false;
-          }
-        }
+        message: 'What are your requirements for accepting contributions?',           
+        
       },
+    ]
 
+ 
+
+  
 
 
 // TODO: Create a function to write README file
-
-
-function writeToFile(fileName, data) {}
-
-const writeFile = fileContent => {
-  return new Promise((resolve, reject) => {
-      fs.writeFile('./dist/index.html', fileContent, err => {
-          //if theer is an error reject the Promise and send the error to the '.catch()' method
-          if (err) {
-              reject(err);
-              //return out of the function here to make sure the Promise doesn't accidently execute the resolve function
-             return; 
-          }
-          //if all is well, reolve the PRomise and send data to the '.then' method
-          resolve({
-              ok: true,
-              message: 'File created!'
-          });
-      });
-  });
-};
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-//init();Questions()
- //   .then(promptProject)
- //   .then(portfolioData => {      
-//return  generatePage(portfolioData);
- //   })
- //   .then(pageHTML => {
- //     return writeFile(pageHTML);
- //   })
- //   .then(writeFileResponse => {
-  //    console.log(writeFileResponse);
-  //    return copyFile;
- //   })
- //   .then(copyFileResponse => {
-  //    console.log(copyFileResponse);
-  //  })
-  //  .catch(err => {
-  //    console.log(err);
-    
+function writeToFile(filename, data) {
+  fs.writeFile(filename, data, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("README created!");
+  })
 }
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions)
+  .then(function(data) {
+    writeToFile("README.md");
+    
+  })
+}
+// Function call to initialize app
+
+init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,6 @@
+var md = require("node-markdown").Markdown;
+var html = md("**markdown** string");
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -10,10 +13,55 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// TODO: Create a function to generate markdown for README ${response.title}
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # Repository Title
+  
+  https://github.com/${data.Username}/${data.title}
+  
+  # GitHub Link
+  
+  
 
+  # Descripition
+
+  ${data.description}
+
+  # Table of Contents
+  -[description](#description)
+  -[installation](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Contributing](#contributing)
+  -[Tests](#tests)
+  -[Questions](#questions)
+  -[Contact Information](#contact)
+
+  #Installation
+
+  ${data.installation}
+
+  #Usage
+
+  ${data.usage}
+
+  #License
+
+  ${data.license}
+
+  #Contributing
+
+  Contributors: ${data.contributing}
+
+  #Tests
+
+  ${data.tests}
+
+  #Questions
+
+  If you have any questions please open an issue in GitHub or contact ${data.Username} at ${data.email}.
 `;
 }
 
